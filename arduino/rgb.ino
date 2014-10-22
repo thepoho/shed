4e86af9921565056c476a16cafa35e4ba75d05c1
poho <thepoho@gmail.com>
2014-10-22 16:28:35 +1100

    adding first untested cut of RGB LED code

---
+++ arduino/rgb.ino
@@ -0,0 +1,39 @@
+#define CLOCK_PIN 8
+#define DATA_PIN 9
+#define LIGHT_COUNT = 50 //the number of lights in the string
+#define SLEEP = 100  //time to sleep between flushes in milllis
+char r, g, b;
+
+void setup()   {
+  pinMode(clock_pin,  OUTPUT);
+  pinMode(data_pin, OUTPUT);
+  r = 255;
+  g = b = 0;
+}
+
+void loop() {
+  while(g++ < 255)
+    flushAndSleep();
+  while(r-- > 0)
+    flushAndSleep();
+  while(b++ < 255)
+    flushAndSleep();
+  while(g-- > 0)
+    flushAndSleep();
+  while(r++ < 255)
+    flushAndSleep();
+}
+
+void flushAndSleep(){
+  flushLights();
+  delay(sleep);
+}
+
+void flushLights(){
+  for(int i=0; i < LIGHT_COUNT; i++){
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, r);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, g);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, b);
+  }
+  delayMicroseconds(500); //500 micros to latch which is apparently 0.5 millis
+}
